104e12f92055b4db07676463249a16e71b27b28d
Hadi1402 <hadisaedi470@gmail.com>
2025-02-08 20:56:18 +0330

    Serializer and urls.products

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'products',
 ]
 
@@ -119,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 #media
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 
